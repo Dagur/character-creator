e420967f718b30f08e269e01346687f3167d3c28
Dagur Páll Ammendrup <dagurp@gmail.com>
2023-01-21 15:09:47 +0000

    Solution

---
+++ src/components/BackgroundBar/BackgroundBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./BackgroundBar.module.css";
+
+const Footer = () => {
+  return <span className={styles.backgroundBar} />;
+};
+
+export default Footer;
